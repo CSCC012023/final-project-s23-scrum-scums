fcaeb7dec27b8f984f6fea210c910d5d03bbd232
arnavp103 <arnavp103@gmail.com>
2023-06-01 01:01:07 -0400

    test tweeting page

--- app/(site)/inscribe/page.tsx
+++ app/(site)/inscribe/page.tsx
@@ -6,50 +6,16 @@ import axios from "axios";
 import { useState } from "react";
 
 const Inscribe = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [email, setEmail] = useState("");
-
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log("submitting", username, password, email);
-		const { data } = await axios.post("/api/inscribe", { username, password, email });
-		setUsername("");
-		setPassword("");
-		setEmail("");
+		console.log("tweeting");
+		await axios.post("/api/inscribe");
 	};
 
 	return (
 		<div>
 			<form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
-				<label className="text-3xl font-bold text-left px-2">Username</label>
-				<input
-					className="border-2 border-black rounded-lg"
-					type="text"
-					name="username"
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-					required
-				/>
-				<label className="text-3xl font-bold text-left px-2">E-mail</label>
-				<input
-					className="border-2 border-black rounded-lg"
-					type="text"
-					name="email"
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					required
-				/>
-				<label className="text-3xl font-bold text-left px-2">Password</label>
-				<input
-					className="border-2 border-black rounded-lg"
-					type="password"
-					name="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					required
-				/>
-				<button type="submit" className="border-2 border-black rounded-lg" >Submit</button>
+				<button type="submit" className="border-2 border-black rounded-lg">Submit</button>
 			</form>
 		</div>
 	);
