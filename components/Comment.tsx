c720cfc836269910d723e64c3e107499f70bc518
Jason Xia <jasonxia2016@gmail.com>
2023-06-15 07:18:02 -0400

    testing new like schema

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -17,17 +17,17 @@ export interface CommentProps {
 }
 
 const Comment = ({ content, author, createdAt }: CommentProps) => {
-    return(
-        <div className="p-4">
-            <div className="flex flex-row justify-between bg-gray-200 flex-wrap">
-                <div className="basis-full flex flex-row justify-between border-b-2 border-gray-600">
-                    <div className="text-sm">{author?.username}</div>
-                    <div className="text-xs">{createdAt.toLocaleString()}</div>
-                </div>
-                <ReactMarkdown className="prose lg: prose-base" children={content}/>
-            </div>
-        </div>
-    );
+	return(
+		<div className="p-4">
+			<div className="flex flex-row justify-between bg-gray-200 flex-wrap">
+				<div className="basis-full flex flex-row justify-between border-b-2 border-gray-600">
+					<div className="text-sm">{author?.username}</div>
+					<div className="text-xs">{createdAt.toLocaleString()}</div>
+				</div>
+				<ReactMarkdown className="prose lg: prose-base" >{content}</ReactMarkdown>
+			</div>
+		</div>
+	);
 };
 
 export default Comment; 
