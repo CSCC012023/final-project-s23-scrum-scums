a020e7ae23f32b19c498b0f13ff7dfa8c7e223c1
arnavp103 <arnavp103@gmail.com>
2023-06-16 17:59:57 -0400

    added react-markdown and an editor to write posts

--- app/(site)/inscribe/page.tsx
+++ app/(site)/inscribe/page.tsx
@@ -3,18 +3,29 @@
 import React from "react";
 // import "@styles/globals.css";
 import axios from "axios";
+import Editor from "@components/Editor";
+import { useState } from "react";
 
 const Inscribe = () => {
+
+	const [text, setText] = useState("Start writing here...");
+
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log("tweeting");
-		await axios.post("/api/inscribe");
+		console.log("posting");
+		const title = text.includes("\n") ? text.split("\n")[0] : text;
+		const content = text;
+		const authorId = "46e1ce09-78e7-4d1b-ba49-8479de96ea76";
+		await axios.post("/api/inscribe", { title, content, authorId });
+		setText("Start writing here...");
 	};
 
+
 	return (
-		<div>
-			<form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
-				<button type="submit" className="border-2 border-black rounded-lg">Submit</button>
+		<div className="w-full h-full ">
+			<Editor type="inscription" text={text} onTextChange={setText}/>
+			<form onSubmit={handleSubmit} className="flex flex-col items-center justify-center mt-4">
+				<button type="submit" className="border-2 border-black rounded-lg p-2 bg-green-300 font-robotoslab">Submit</button>
 			</form>
 		</div>
 	);
--- components/Inscription.tsx
+++ components/Inscription.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import ReactMarkdown  from "react-markdown";
 import React from "react";
 // import Image from "next/image";
 
@@ -30,14 +29,14 @@ const Inscription = ({
 }: InscriptionProps
 ) => {
 	return (
-		<section className="rounded-md  flex flex-col w-full h-full justify-between p-4">
+		<section className="rounded-md flex flex-col w-full h-full justify-between bg-slate-100">
 			<div className="header flex bg-gray-200 flex-col items-start gap-2 p-2">
 				<div className="h-2"> { title } </div>
 				<div className="text-sm">{ author?.username }</div>
 				<div className="text-xs">{ createdAt.toLocaleString() }</div>
 			</div>
 			{/* <Image src={} alt="user photo" /> */}
-			<div className="font-sans mt-4"> { content } </div>
+			<div className="font-robotoslab mt-4 font-normal p-2"> <ReactMarkdown children={content}/> </div>
 		</section>
 	);
 };
