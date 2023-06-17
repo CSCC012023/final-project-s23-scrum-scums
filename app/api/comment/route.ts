33cf35d55cfe285765d715c09bb6b9561edfd60c
duccie42 <76762011+arwinfong@users.noreply.github.com>
2023-06-16 23:26:17 -0400

    fixed: package.json and other bugs

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -4,14 +4,14 @@ const prisma = new PrismaClient();
 
 export const POST = async (request: Request) => {
     const { inscribeId, content } = await request.json();
-    const authorId = "1393486f-78b9-44c2-9282-1af8b75dc88e";
+    const authorId = "46e1ce09-78e7-4d1b-ba49-8479de96ea76";
     const comment = await prisma.comment.create({
         data: {
             inscribeId: inscribeId,
             content: content,
             authorId: authorId,
             createdAt: new Date(),
-            likes: 0,
+            // likes: 0,
         },
     });
     return new Response(JSON.stringify(comment), { status: 201 });
