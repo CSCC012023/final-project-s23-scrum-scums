427513fb4fbf966b5aa2a9c9e54042181fd8b0a3
arnavp103 <arnavp103@gmail.com>
2023-07-05 03:01:31 -0400

    swapped to test db to test somethings

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,11 @@ import NextAuth from "next-auth";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import prisma from "@lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@lib/prisma";
 
 const authConfig: NextAuthOptions = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -16,21 +17,6 @@ const authConfig: NextAuthOptions = {
 			clientSecret: process.env.GITHUB_CLIENT_SECRET as string
 		})
 	],
-	session: {
-		// Use JWT instead of database sessions
-		strategy: "jwt",
-	},
-	// cookies: {
-	// 	sessionToken: {
-	// 		name: "next-auth.session-token",
-	// 		options: {
-	// 			httpOnly: true,
-	// 			sameSite: "strict", // This can be lax or strict, you might want to change this to 'strict' for more security
-	// 			path: "/",
-	// 			secure: process.env.NODE_ENV === "production", // Ensures the cookie is only sent over HTTPS
-	// 		},
-	// 	},
-	// },
 };
 
 const handler = NextAuth(authConfig);
