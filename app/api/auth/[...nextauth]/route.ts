837b6b54493cea5c5a9e2742a2f1cd3e1aca27b1
Harrick Cheong <harrickcheong@gmail.com>
2023-07-05 18:03:54 -0400

    prisma adapter for sessions, and some client sided authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,14 @@ import NextAuth from "next-auth";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 // import prisma from "@lib/prisma";
+import {	PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const authConfig: NextAuthOptions = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -20,17 +24,15 @@ const authConfig: NextAuthOptions = {
 		// Use JWT instead of database sessions
 		strategy: "jwt",
 	},
-	// cookies: {
-	// 	sessionToken: {
-	// 		name: "next-auth.session-token",
-	// 		options: {
-	// 			httpOnly: true,
-	// 			sameSite: "strict", // This can be lax or strict, you might want to change this to 'strict' for more security
-	// 			path: "/",
-	// 			secure: process.env.NODE_ENV === "production", // Ensures the cookie is only sent over HTTPS
-	// 		},
-	// 	},
-	// },
+	callbacks: {
+		async jwt({ token, user }) {
+			return {...token, ...user};	
+		},
+		async session({ session, token, user}){
+			session.user = token as any;
+			return session;
+		},
+	}
 };
 
 const handler = NextAuth(authConfig);
