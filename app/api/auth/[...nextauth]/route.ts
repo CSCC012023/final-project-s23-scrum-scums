4e2f7860c1d7af2efd20f5cf1512ebbed213764d
Eric <fcweric@gmail.com>
2023-07-06 20:06:45 -0400

    Add rudimentary api docs and change login to creds

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import axios from "axios";
 
 export const authConfig: NextAuthOptions = {
 	// @ts-expect-error - adapter randomly can't accept undefined even though that's its
@@ -16,6 +18,32 @@ export const authConfig: NextAuthOptions = {
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID as string,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+		}),
+		CredentialsProvider({
+			name: "Email",
+			credentials: {
+				email: { label: "Email", type: "email", placeholder: "Email" },
+				username: { label: "Username", type: "text", placeholder: "Username" },
+				password: { label: "Password", type: "password", placeholder: "Password"}
+			},
+			async authorize(credentials) {
+				const res = await axios.post("/api/signup", {
+					email: credentials?.email,
+					username: credentials?.username,
+					password: credentials?.password
+				});
+				console.log("submitting", credentials?.username, credentials?.password, credentials?.email);
+
+				const user = await res.data.json();
+
+				if (user) {
+					console.log("Logged in as ", credentials?.username);
+					return user;
+				} else {
+					console.log("Login failed");
+					return null;
+				}
+			}
 		})
 	],
 	callbacks: {
@@ -24,7 +52,7 @@ export const authConfig: NextAuthOptions = {
 				session.user.id = user.id;
 			}
 			return session;
-		}
+		},
 	},
 };
 
