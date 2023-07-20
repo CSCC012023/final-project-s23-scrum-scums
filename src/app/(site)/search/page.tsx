275615251b9e34b40781141f613881c8e1d03f6d
JasonXia2000 <39924803+JasonXia2000@users.noreply.github.com>
2023-07-20 05:06:04 -0400

    Feature/obelisk 33 (#13)
    * added search functionality for hashtags, text
    
    * added a bit of styling
    
    * added author username
    
    * ran npx prisma migrate dev to include the sql migration for our database
    
    * org: ran npx prisma migrate dev to include the sql migration for our database. moved code into src/ folder to clean up everything.
    
    * fix: removed a typo that was causing search to break and updated the api code to use next 13 server functions
    
    ---------
    
    Co-authored-by: arnavp103 <arnavp103@gmail.com>

---
+++ src/app/(site)/search/page.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/jsx-key */
+"use client";
+import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import useSWR from "swr";
+import { Post } from "@prisma/client";
+
+const fetchPosts = async (url: string) => {
+	const response = await fetch(url);
+
+	if (!response.ok) {
+		throw new Error("Failed to fetch posts");
+	}
+
+	return response.json();
+};
+
+const SearchPage = () => {
+	const search = useSearchParams();
+	const searchQuery = search ? search?.get("q") : null;
+	const router = useRouter();
+
+	const encodedSearchQuery = encodeURI(searchQuery || "");
+
+	const { data } = useSWR(
+		`/api/search?q=${encodedSearchQuery}`,
+		fetchPosts,
+		{ revalidateOnFocus: false }
+	);
+
+	if (!encodedSearchQuery) {
+		router.push("/");
+	}
+
+	if(!data?.posts) {
+		return null;
+	}
+	if (data.posts.length === 0) {
+		return (
+			<div>
+				No results found
+			</div>
+
+		);
+	}
+	return (
+		<div>
+			{data.posts.map((Post: Post) => (
+				<div className="hover:bg-slate-900 p-3 gap-4 my-4 rounded-xl border-[2px] border-zinc-600">
+					{Post.title}
+					<div className="flex flex-row flex-wrap gap-6 w-3/5">
+						{Post.content}
+						<div>
+							By {Post.author.username}
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+
+};
+
+export default SearchPage;
