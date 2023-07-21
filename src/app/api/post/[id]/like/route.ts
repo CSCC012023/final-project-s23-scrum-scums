f860ba3faf2b9886f5c2cb14e5b1aaf4268e7e13
Eric <fcweric@gmail.com>
2023-07-21 16:33:49 -0400

    Feat: Add unlike functionality to like button
    Icon does not change from outline to fileed and vice versa in real time

--- src/app/api/post/[id]/like/route.ts
+++ src/app/api/post/[id]/like/route.ts
@@ -2,17 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@src/lib/prisma";
 
 interface body {
-	[userId: string]: string
+	[userId: string]: string;
 }
 export const POST = async (req: NextRequest, 
 	{ params }: { params: { id: string } }) => {
 	const id = parseInt(params.id);
 	const data: body = await req.json();
 	const userId = data.userId;
+	const liked = data.isLiked;
+	console.log("liked", liked)
 	console.log("userid", userId);
 	console.log("id", id);
 
-	const likes = await prisma.post.update({	
+	const likes = liked
+		? await prisma.post.update({	
 		where: { 
 			id: id 
 		},
@@ -26,7 +29,16 @@ export const POST = async (req: NextRequest,
 		select: {
 			likes: true
 		}
+	})
+	: await prisma.postLike.delete({
+		where: {
+			userId_postId: {
+				userId: userId,
+				postId: id
+			}
+		}
 	});
+
 	// console.log("likes", likes);	
 	return NextResponse.json(likes);
 }
